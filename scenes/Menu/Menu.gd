a6bd6da4cd6e35e77d71b4c0ed63d4c0037d3330
lpancho <leonard.pancho@gmail.com>
2019-09-22 10:20:37 +0800

    menu play click sound

--- scenes/Menu/Menu.gd
+++ scenes/Menu/Menu.gd
@@ -3,7 +3,7 @@ extends Control
 onready var anim_gui = $Anim_GUI
 onready var fish_1 = $obj_fish 	# the fish that face to the right after animation
 onready var fish_2 = $obj_fish2 # the fish that faces to the left after animation
-
+onready var play_click = $play_click
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	anim_gui.play("show_fishes")
@@ -18,5 +18,6 @@ func _on_Anim_GUI_animation_finished(anim_name):
 	pass # Replace with function body.
 	
 func _on_tb_play_pressed():
+	play_click.play()
 	anim_gui.play("start_game")
 	pass # Replace with function body.
